44feb548c65525d67b9ec5000b89ef763a73a3b2
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-23 09:30:04 -0500

    added Add component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Add from './components/Add';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/add" element={<Add />} />
       </Routes>
     </BrowserRouter>
   );
