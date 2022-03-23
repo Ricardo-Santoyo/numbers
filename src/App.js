b4b68e22371cddc4204c0d1e0b97653f470b8eb4
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-23 17:41:18 -0500

    added Random component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Add from './components/Add';
+import Random from './components/Random';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/add" element={<Add />} />
+        <Route exact path="/add/random" element={<Random sign="+" />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Random.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Random(props) {
+
+  return (
+    <div>
+      <h1 className="Header">Random</h1>
+    </div>
+  );
+}
+
+export default Random;
