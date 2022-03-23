d4726c6d4dc13e5b2d9f28e92099e33df0d9ea32
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-22 21:56:36 -0500

    added add function to add.js

--- src/__tests__/add.test.js
+++ src/__tests__/add.test.js
@@ -1,1 +1,13 @@
 import add from '../mathFunctions/add';
+
+test('returns the sum of 2 numbers', () => {
+    expect(add(7, 26)).toBe(33);
+});
+
+test('returns the sum of 2 negative numbers', () => {
+  expect(add(-67, -88)).toBe(-155);
+});
+
+test('returns the sum of a positive and negative number', () => {
+  expect(add(-4, 23)).toBe(19);
+});
