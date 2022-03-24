9f12182955bad19dd3fbaf35388bb886cb6d41ee
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-23 19:32:41 -0500

    added handleClick function in Numpad component

--- src/components/Numpad.js
+++ src/components/Numpad.js
@@ -3,19 +3,26 @@ import React from 'react';
 function Numpad() {
   const numpad = [[7,8,9], [4,5,6], [1,2,3], [0]]
 
+  function handleClick(num) {
+    console.log(num);
+  }
+
   return (
     <div>
       <div className="Numpad">
-        {numpad[0].map((i) => (<span key={i}>{i}</span>))}
+        {numpad[0].map((n) => (<span key={n} onClick={() => handleClick(n)}>{n}</span>))}
       </div>
+
       <div className="Numpad">
-        {numpad[1].map((i) => (<span key={i}>{i}</span>))}
+        {numpad[1].map((n) => (<span key={n} onClick={() => handleClick(n)}>{n}</span>))}
       </div>
+
       <div className="Numpad">
-        {numpad[2].map((i) => (<span key={i}>{i}</span>))}
+        {numpad[2].map((n) => (<span key={n} onClick={() => handleClick(n)}>{n}</span>))}
       </div>
+
       <div className="Numpad">
-        {numpad[3].map((i) => (<span key={i}>{i}</span>))}
+        {numpad[3].map((n) => (<span key={n} onClick={() => handleClick(n)}>{n}</span>))}
       </div>
     </div>
   );
