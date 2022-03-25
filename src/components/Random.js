86ea115db38ba37cf75187125c0c671724fdebf2
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-25 18:44:30 -0500

    refactored function nextQuestion in Random component

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,33 +1,32 @@
 import React, { useState } from 'react';
 import Numpad from './Numpad';
+import randomNumArray from '../mathFunctions/randomNumArray';
 
 function Random(props) {
-  const [num1, setNum1] = useState(Math.floor(Math.random() * 100));
-  const [num2, setNum2] = useState(Math.floor(Math.random() * 100));
+  const [nums, setNums] = useState(randomNumArray(2));
   const [input, setInput] = useState('');
 
   function nextQuestion() {
     // Displays the next question on the screen.
-    setNum1(Math.floor(Math.random() * 100));
-    setNum2(Math.floor(Math.random() * 100));
+    setNums(randomNumArray(2));
     setInput('');
   }
 
   function checkResponse() {
     // Checks whether input is the correct answer or not.
-    let answer = props.operation(num1, num2);
+    let answer = props.operation(nums[0], nums[1]);
     if (input == answer) {
       setInput("Correct");
     } else {
       setInput("Incorrect");
     }
-    setTimeout(nextQuestion, 3000);
+    setTimeout(nextQuestion, 2000);
   }
 
   return (
     <div>
       <h1 className="Header">Random</h1>
-      <h2 className="Question">{`${num1} ${props.sign} ${num2} = ?`}</h2>
+      <h2 className="Question">{`${nums[0]} ${props.sign} ${nums[1]} = ?`}</h2>
       <div className="Input">
         <h2>{input}</h2>
       </div>
