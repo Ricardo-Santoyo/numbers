b403488560f940e7efa1712a553bb655844e2e15
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-03-25 23:11:26 -0500

    added Subtract route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Add from './components/Add';
+import SelectMode from './components/SelectMode';
 import Random from './components/Random';
 import add from './mathFunctions/add';
 
@@ -10,8 +10,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/add" element={<Add />} />
+        <Route exact path="/add" element={<SelectMode operation="Add" />} />
         <Route exact path="/add/random" element={<Random sign="+" operation={add} />} />
+        <Route exact path="/subtract" element={<SelectMode operation="Subtract" />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/SelectMode.js
+++ src/components/SelectMode.js
@@ -3,11 +3,11 @@ import ChoiceBlock from './ChoiceBlock';
 import { ReactComponent as ShuffleIcon } from '../icons/shuffle.svg';
 import { ReactComponent as TableIcon } from '../icons/table.svg';
 
-function Add() {
+function SelectMode(props) {
 
   return (
     <div className="SelectMode">
-      <h1 className="Header">Add</h1>
+      <h1 className="Header">{props.operation}</h1>
       <div className="Container">
         <ChoiceBlock symbol={<ShuffleIcon />} name ="Random" />
         <ChoiceBlock symbol={<TableIcon />} name ="Tables" />
@@ -16,4 +16,4 @@ function Add() {
   );
 }
 
-export default Add;
+export default SelectMode;
