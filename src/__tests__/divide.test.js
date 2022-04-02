3dbe3a2a38c645c761feeeeaa9f9b5e4b4f2ed2a
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-04-02 06:00:39 -0500

    fixed bug in divide.js that was not rounding to the correct 100ths place.

--- src/__tests__/divide.test.js
+++ src/__tests__/divide.test.js
@@ -8,10 +8,18 @@ test('returns the quotient of 2 numbers(2)', () => {
   expect(divide(15, 1000)).toBe(0.02);
 });
 
+test('returns the quotient of 2 numbers(2)', () => {
+  expect(divide(81, 22)).toBe(3.68);
+});
+
 test('returns the quotient of 2 negative numbers', () => {
   expect(divide(-69, -3)).toBe(23);
 });
 
-test('returns the quotient of a positive and negative number', () => {
+test('returns the quotient of a positive and negative number(1)', () => {
   expect(divide(-4, 23)).toBe(-0.17);
+});
+
+test('returns the quotient of a positive and negative number(2)', () => {
+  expect(divide(-12, 34)).toBe(-0.35);
 });
