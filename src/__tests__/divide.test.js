f91ab9583e9e725f9d7461e210f0bea439060d5a
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-04-02 05:12:40 -0500

    added divide.js math fucntion

---
+++ src/__tests__/divide.test.js
@@ -0,0 +1,17 @@
+import divide from '../mathFunctions/divide';
+
+test('returns the quotient of 2 numbers(1)', () => {
+    expect(divide(7, 26)).toBe(0.27);
+});
+
+test('returns the quotient of 2 numbers(2)', () => {
+  expect(divide(15, 1000)).toBe(0.02);
+});
+
+test('returns the quotient of 2 negative numbers', () => {
+  expect(divide(-69, -3)).toBe(23);
+});
+
+test('returns the quotient of a positive and negative number', () => {
+  expect(divide(-4, 23)).toBe(-0.17);
+});
---
+++ src/mathFunctions/divide.js
@@ -0,0 +1,7 @@
+function divide(num1, num2) {
+  // returns the quotient of two numbers. rounds to the nearest 100ths place.
+  const result = num1 / num2;
+  return Math.ceil(result * 100) / 100
+};
+
+export default divide;
