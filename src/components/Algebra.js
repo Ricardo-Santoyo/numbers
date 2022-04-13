df6eb34a6c034e50d1c935f5054d478db40aa00f
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2022-04-12 20:41:31 -0500

    added options to Algebra.js component

--- src/components/Algebra.js
+++ src/components/Algebra.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ChoiceBlock from './ChoiceBlock';
 
 function Algebra() {
 
   return (
     <div className="Algebra">
       <h1 className="Header">Algebra</h1>
+      <div className="Container xsmall">
+        <ChoiceBlock symbol="2𝑥 + 1" name ="Expressions" link="expressions" />
+        <ChoiceBlock symbol="(𝑥+3) (𝑥-2)" name ="Binomials" link="binomials" />
+      </div>
     </div>
   );
 }
